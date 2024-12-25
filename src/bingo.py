74494244ea345349df58284c9a1af3c0bad75d31
Hirad Asadi <hirad.asadi@gmail.com>
2024-12-25 02:05:56 +0100

    wip

--- src/bingo.py
+++ src/bingo.py
@@ -1,15 +1,54 @@
 import timeit
 import bingo_functions as bf
+import random
 
+def generate_game_cards(nr_of_cards: int, seed_boards: list[bf.BingoBoard], 
+                        prize_distribution: bf.PrizeDistribution) -> list[bf.BingoCard]:
+    cards = []
+    
+    row_index = 0
+    for prize in prize_distribution.get_prizes():
+        if prize == 0:
+            continue
+        
+        board = random.choice(seed_boards)
+        prize_row = [
+                    board.get_board_data()[0][row_index], 
+                    board.get_board_data()[1][row_index], 
+                    board.get_board_data()[2][row_index], 
+                    board.get_board_data()[3][row_index],
+                    board.get_board_data()[4][row_index]
+                ]
+        
+        # Store the prize row for lottery
+        # Create nr_of_cards * probability of winning rows copies of the board
+        
+    
+        
+        row_index += 1
+        
+        
+    return cards
     
 if __name__ == "__main__":
     print("Bingo Game\n")
     
-    nr_of_boards = 10000
-    boards = []
-    start_time = timeit.default_timer()
-    boards = bf.generate_boards(nr_of_boards)
-    elapsed_time = timeit.default_timer() - start_time
+    prize_distribution = bf.PrizeDistribution()
     
-    print(f"Time taken to generate {nr_of_boards} boards: {elapsed_time:.2f} seconds")
-    print(f"Unique boards generated: {len(boards)}")
+    prize_distribution.add_prize(500, 0.0002)
+    prize_distribution.add_prize(1000, 0.0001)
+    prize_distribution.add_prize(15000, 0.00002)
+    prize_distribution.add_prize(150000, 0.00001)
+    prize_distribution.add_prize(1000000, 0.000001)
+    
+    nr_of_seed_boards = 10000
+    seed_boards = []
+    
+    #start_time = timeit.default_timer()
+    seed_boards = bf.generate_boards(nr_of_seed_boards)
+    #elapsed_time = timeit.default_timer() - start_time
+    
+    #print(f"Time taken to generate {nr_of_seed_boards} boards: {elapsed_time:.2f} seconds")
+    #print(f"Unique boards generated: {len(seed_boards)}")
+    
+    generate_game_cards(1000000, seed_boards, prize_distribution)
--- src/bingo_functions.py
+++ src/bingo_functions.py
@@ -1,87 +1,102 @@
 from hashlib import sha256
 import random
 
-# Generate board a 15 x 5, three separate  5x5 board grids with unique number per cell and column
-def generate_board():
-    bColumn = generate_random_numbers(1, 15)
-    iColumn = generate_random_numbers(16, 30)
-    nColumn = generate_random_numbers(31, 45)
-    gColumn = generate_random_numbers(46, 60)
-    oColumn = generate_random_numbers(61, 99)      
-    
-    return [bColumn, iColumn, nColumn, gColumn, oColumn]
 
-
-def print_board(board):
-    
-    print("B\tI\tN\tG\tO\n")
-    
-    for x in range(15):
-        print(board[0][x] , "\t", board[1][x] , "\t", board[2][x] , 
-              "\t", board[3][x] , "\t", board[4][x])
-        if (x+1) % 5 == 0:
-            print("\n")
-            
-# TODO:  this might have a infinite loop if it can't find a unique number, 
-# don't set a low max number in combination with a high array size
-def generate_random_numbers(min_number, max_number):
-    
-    arr = [int] * 15
-    used_numbers = {}
-    
-    for x in range(len(arr)):      
-        not_used = True
-        
-        while not_used:
-            rand_nr = random.randint(min_number, max_number)
-            
-            if rand_nr not in used_numbers:
-                arr[x] = rand_nr
-                used_numbers[rand_nr] = True
-                not_used = False
-    return arr
-
-
-def generate_boards(nr_of_boards):
-    checksum_dict = {}
-    boards = []
+def generate_boards(nr_of_boards: int) -> list['BingoBoard']:
+    checksum_dict: dict[str, bool] = {}
+    boards = [BingoBoard]
     
     for x in range(nr_of_boards):
         not_found = True
         
         while not_found:
-            board = generate_board()
-            checksum = sha256(str(board).encode()).hexdigest()
+            board = BingoBoard()
+            checksum = board.get_checksum()
             
             if checksum not in checksum_dict:
                 checksum_dict[checksum] = True
                 not_found = False
-                boards.append(BingoBoard(board, checksum))
+                boards.append(board)
                 
     return boards
             
 
 class BingoBoard:
-    board = []
-    checksum = ""
+    __board_data = [list[int]]
+    __checksum = ""
     
-    def __init__(self, board, checksum):
-        self.board = board
-        self.checksum = checksum
+    def __init__(self):
+        self.__board_data = self.__generate_board_data()
+        self.__checksum = sha256(str(self.__board_data).encode()).hexdigest()
+            
+    # Generate board a 15 x 5, three separate  5x5 board grids with unique number per cell and column
+    def __generate_board_data(self) -> list[list[int]]:
+        bColumn = self.__generate_random_numbers(1, 15)
+        iColumn = self.__generate_random_numbers(16, 30)
+        nColumn = self.__generate_random_numbers(31, 45)
+        gColumn = self.__generate_random_numbers(46, 60)
+        oColumn = self.__generate_random_numbers(61, 99)      
+        
+        return [bColumn, iColumn, nColumn, gColumn, oColumn]
     
-    def get_checksum(self):
-        return self.checksum
+    # TODO: this might have a infinite loop if it can't find a unique number, 
+    # don't set a low max number in combination with a high array size
+    def __generate_random_numbers(self, min_number: int, max_number: int) -> list[int]:
+        
+        arr = [int] * 15
+        used_numbers: dict[int, bool] = {}
+        
+        for x in range(len(arr)):      
+            not_used = True
+            
+            while not_used:
+                rand_nr = random.randint(min_number, max_number)
+                
+                if rand_nr not in used_numbers:
+                    arr[x] = rand_nr
+                    used_numbers[rand_nr] = True
+                    not_used = False
+        return arr
+        
+    def get_checksum(self) -> str:
+        return self.__checksum
     
-    def get_board(self):
-        return self.board
+    def get_board_data(self) -> list[list[int]]:
+        return self.__board_data
+    
     
 class BingoCard:
-    boards = []       
+    __boards = [BingoBoard]       
         
-    def __init__(self, boards):
-        self.boards = boards
+    def __init__(self, boards: list[BingoBoard]):
+        self.__boards = boards
     
-    def get_boards(self):
-        return self.boards
+    def get_boards(self) -> list[BingoBoard]:
+        return self.__boards
         
+        
+class PrizeDistribution: 
+    __prize_distribution: dict[int, float] = { }
     
+    def add_prize(self, prize: int, probability: list[int]) -> None:
+        
+        if prize == 0:
+            raise ValueError("Prize can't be 0")
+        
+        self.__prize_distribution[prize] = probability
+        
+        win_probability_sum = 0
+        
+        # Calculate the probability of winning something
+        for key in self.__prize_distribution:
+            if key != 0:
+                win_probability_sum += self.__prize_distribution[key]
+                
+        # 0 is the probability of not winning anything
+        self.__prize_distribution[0] = 1 - win_probability_sum 
+    
+    def get_probability(self, prize: int) -> float:
+        return self.__prize_distribution[prize]
+    
+    def get_prizes(self) -> list[int]:
+        return sorted(self.__prize_distribution.keys())
